111b66ef85ce25f753066a5ca7c088f4a1eb6c94
nadaelmahfoudi <elmahfoudinada17@gmail.com>
2023-10-23 16:19:47 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// variables var, let , const
+// var vs const // local scope and global scope
+// syntax
+// datatypes (primitive datatype non-primitive datatype)
+// objects
+// loops in js : forEach // for .. in // for .. of
+// functions: anonymous func / simple function / arrow function (arg) => {}
+
+icons.addEventListener("click", () => {
+    nav.classList.toggle("active");
+})
+function toggleSearch() {
+    var input = document.getElementById("search-input");
+    if (input.style.display === "none") {
+        input.style.display = "block";
+    } else {
+        input.style.display = "none";
+    }
+}
+
+  
