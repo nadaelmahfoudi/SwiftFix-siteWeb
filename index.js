848379ed284e89a132b0282d8c2681550699f8ac
nadaelmahfoudi <elmahfoudinada17@gmail.com>
2023-10-26 12:12:25 +0200

    adding testimonial-carousel and services-modals

--- index.js
+++ index.js
@@ -68,3 +68,28 @@ function validateForm() {
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
+//le testimonial Carousel
+
+var slideIndex = 0;
+  carousel();
+
+  function carousel() {
+      var i;
+      var x = document.getElementsByClassName("testimonial");
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > x.length) {slideIndex = 1}    
+      x[slideIndex-1].style.display = "block";  
+      setTimeout(carousel, 2000); 
+  }
+//ouvrir le Modal
+  function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
